1ea7108ce0d39f7e8e00535e2f31824e7d332364
Hugo Vinicius <hugovinicius145@gmail.com>
2021-11-08 15:46:30 -0300

    Header Responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,22 @@
 import { Text, Flex, Box, Avatar} from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right" >
-        <Text>Hugo Vinicius</Text>
-        <Text 
-          color="gray.300"
-          fontSize="small"
-        >
-          hvls36@gmail.com</Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right" >
+          <Text>Hugo Vinicius</Text>
+          <Text 
+            color="gray.300"
+            fontSize="small"
+          >
+            hvls36@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Hugo Vinicius" src="https://github.com/hugovinicius145.png" />
     </Flex>
   );

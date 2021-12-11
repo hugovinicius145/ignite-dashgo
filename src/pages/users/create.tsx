d124171f47cc1d52c2ba799e66a077b5e36494be
Hugo Vinicius <hugovinicius145@gmail.com>
2021-12-11 10:57:51 -0300

    Utilizando Mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,11 +2,15 @@ import Link from 'next/link';
 import { Box, Flex, Button, VStack, HStack, Text, SimpleGrid, Heading, Divider } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { useMutation } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Input } from '../../components/Form/Input';
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -26,6 +30,23 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
@@ -33,8 +54,9 @@ export default function CreateUser() {
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(values)
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   }
 
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,7 +18,7 @@ export default function UserList() {
     lg: true,
   });
 
-  async function handlePrefetchUser(userId: number) {
+  async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(['user', userId], async () => {
       const response = await api.get(`users/${userId}`);
 
@@ -84,7 +84,7 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(Number(user.id))} href="">
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)} href="">
                               <Text fontWeight="bold">{user.name}</Text>                            
                             </Link>                            
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>

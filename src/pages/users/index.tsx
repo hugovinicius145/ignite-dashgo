d3d0f4d0ec72f16e2a4de1afb27d0699e0939f7e
Hugo Vinicius <hugovinicius145@gmail.com>
2021-12-11 11:47:19 -0300

    SSR no React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,13 +4,17 @@ import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import NextLink from 'next/link';
-import { useUsers } from '../../services/hooks/useusers';
+import { getUsers, useUsers } from '../../services/hooks/useusers';
 import { useState } from 'react';
 import { queryClient } from '../../services/queryClient';
 import { api } from '../../services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
+  // const { data, isLoading, isFetching, error } = useUsers(page, {
+  //   initialData: users,
+  // });
   const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
@@ -120,3 +124,13 @@ export default function UserList() {
     </Box>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const { users, totalCount } = await getUsers(1)
+
+//   return {
+//     props: {
+//       users,      
+//     }
+//   }
+// }
